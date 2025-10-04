a764db89d4c2096bae121861df0b9f7d4089b1d9
Дмитро Данилкович <forizz@MacBook-Pro-Dmitro.local>
2025-10-04 17:02:36 +0300

    add QuizProgress component to quiz features

---
+++ src/features/quiz/ui/QuizProgress.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import type { Question } from "~/entities";
+import { Check, ChevronUp } from "lucide-react";
+import clsx from "clsx";
+import { Link } from "react-router";
+import { CheckMark } from "~/widgets";
+
+type QuizProgressProps = {
+  questions: Question[];
+};
+
+function QuizProgress({ questions }: Readonly<QuizProgressProps>) {
+  const [open, setOpen] = useState(true);
+
+  const [progress, setProgress] = useState(
+    questions.map((_question, index) => {
+      return index % 2 === 0;
+    }),
+  );
+
+  function handleOpen() {
+    setOpen((prev) => !prev);
+  }
+
+  return (
+    <section className="bg-accent-background h-fit space-y-2 rounded-md shadow-md">
+      <button
+        type="button"
+        className="inline-flex items-center justify-between gap-8 border-b border-zinc-400 p-2"
+        onClick={handleOpen}
+      >
+        <h3 className="text-lg font-semibold">Quiz Question List</h3>
+        <ChevronUp
+          className={clsx(
+            !open && "rotate-180",
+            "stroke-primary transition-transform",
+          )}
+        />
+      </button>
+      {open && (
+        <ol className="flex flex-col space-y-2 p-2">
+          {questions.map((_question, index) => (
+            <li
+              key={index}
+              className="flex"
+            >
+              <Link
+                to="#"
+                className={clsx(
+                  "bg-muted-background flex flex-1 items-center justify-between rounded-md p-2 text-sm",
+                  progress[index] ? "text-primary bg-primary/10" : "opacity-30",
+                )}
+              >
+                <span>Quiz Question {index + 1}</span>
+
+                {progress[index] && <CheckMark />}
+              </Link>
+            </li>
+          ))}
+        </ol>
+      )}
+    </section>
+  );
+}
+
+export { QuizProgress };
