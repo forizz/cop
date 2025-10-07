31bd93f8e80dea7a248519fa31bafa2855ce021c
Дмитро Данилкович <forizz@MacBook-Pro-Dmitro.local>
2025-10-07 13:24:04 +0300

    styles

--- src/features/categories/ui/CategoryGrid.tsx
+++ src/features/categories/ui/CategoryGrid.tsx
@@ -4,7 +4,7 @@ import { CategoryCard } from "./CategoryCard";
 
 function CategoryGrid({ categories }: { categories: Category[] }) {
   return (
-    <div className="grid max-w-5xl auto-rows-max grid-cols-2 gap-6">
+    <div className="grid max-w-7xl flex-1 auto-rows-max grid-cols-2 gap-6">
       {categories.map((category) => (
         <CategoryCard
           key={category.link}
