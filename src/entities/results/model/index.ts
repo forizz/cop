8013ba8a7730cbc037c27a6bf2804667d99ac960
Дмитро Данилкович <forizz@MacBook-Pro-Dmitro.local>
2025-12-24 14:53:28 +0200

    clean up

--- src/entities/results/model/index.ts
+++ src/entities/results/model/index.ts
@@ -28,4 +28,3 @@ export type ResultStatGame = {
 
 export * from "./store";
 export * from "./store-types";
-export * from "./results";
--- src/features/results/index.ts
+++ src/features/results/index.ts
@@ -1,7 +1,6 @@
 export { StatisticCard } from "./ui/StatisticCard";
 export { StatisticsCardList } from "./ui/StatisticsCardList";
 export { DetailedStats } from "./ui/DetailedStats";
-export { latestGames } from "./model/results";
 export { LatestGames } from "./ui/LatestGames";
 export { BestGames } from "./ui/BestGames";
 export { ResultGame } from "./ui/ResultGame";
